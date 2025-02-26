6b611153aa346dca227bd05e15c41b2d0b16373b
taozc1 <1193471306@qq.com>
2025-02-26 15:55:08 +0800

    add requirement.txt

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
-import mlflow
 if __name__ == '__main__':
-    mlflow.set_tracking_uri('file:./runs')
+    # mlflow.set_tracking_uri('file:./runs')|
     # 加载模型
     model = YOLO("ultralytics/cfg/models/11/yolo11s-model.yaml")
     # model = YOLO("yolo11s-cls.yaml")
@@ -10,7 +9,7 @@ if __name__ == '__main__':
     # 训练模型
     train_results = model.train(
         data="caltech101",  # 数据集 YAML 路径
-        batch = '32',
+        batch = 32,
         epochs=50,  # 训练轮次
         imgsz=416,  # 训练图像尺寸
         device="0",  # 运行设备，例如 device=0 或 device=0,1,2,3 或 device=cpu
